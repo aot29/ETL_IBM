9426bc927383227ce59349ce5b82e35c0fb047d6
Alvaro Ortiz Troncoso <95299082+aot29@users.noreply.github.com>
2025-01-08 18:54:47 +0100

    Update my_first_dag.py

--- my_first_dag.py
+++ my_first_dag.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
-from airflow.utils.dates impor days_ago
+from airflow.utils.dates import days_ago
 
 # paths
 input_file = '/etc/passwd'
